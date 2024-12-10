ff8495c04a09fd9b80cf600bc0a9be332d11fc68
RahulProjects01 <srahulb6@gmail.com>
2024-12-10 14:15:48 +0530

    update code

---
+++ src/main/java/com/springhibernate/SpringBootHibernate1Application.java
@@ -0,0 +1,14 @@
+package com.springhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHibernate1Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootHibernate1Application.class, args);
+	}
+
+}
